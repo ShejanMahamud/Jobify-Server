c4df506270155924345b9ce7d6216d2271bd6812
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-05-22 03:18:29 +0600

    Added CronJobs to review expired jobs on every 12 hrs

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
+const cron = require('node-cron');
+const moment = require('moment')
 const port = process.env.PORT || 3473;
 const mongoURI = process.env.MONGO_URI;
 const secret_token = process.env.ACCESS_TOKEN_SECRET;
@@ -13,10 +14,12 @@ const secret_token = process.env.ACCESS_TOKEN_SECRET;
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -44,6 +47,9 @@ const verifyToken = (req, res, next) => {
   });
 };
 
+
+
+
 // Main function to run MongoDB client and set up routes
 const run = async () => {
   try {
@@ -53,16 +59,19 @@ const run = async () => {
     const companiesCollection = client.db("jobify").collection("companies");
     const usersCollection = client.db("jobify").collection("users");
     const appliedJobsCollection = client.db("jobify").collection("appliedJobs");
-    const bookmarkJobsCollection = client.db("jobify").collection("bookmarkJobs");
+    const bookmarkJobsCollection = client
+      .db("jobify")
+      .collection("bookmarkJobs");
 
+    //get all jobs
     app.get("/jobs", async (req, res) => {
       try {
         let query = {};
-        if(req.query.open_jobs){
-          query = {company_name: req.query.open_jobs}
+        if (req.query.open_jobs) {
+          query = { company_name: req.query.open_jobs };
         }
-        if(req.query.jobId){
-          query = {_id: new ObjectId(req.query.jobId)}
+        if (req.query.jobId) {
+          query = { _id: new ObjectId(req.query.jobId) };
         }
         if (req.query.related && req.query.id) {
           query = {
@@ -77,6 +86,7 @@ const run = async () => {
       }
     });
 
+    //get job count for pagination
     app.get("/jobs_count", async (req, res) => {
       try {
         const result = await jobsCollection.countDocuments();
@@ -86,6 +96,7 @@ const run = async () => {
       }
     });
 
+    //search and pagination [will replace later]
     app.get("/search", async (req, res) => {
       try {
         const { title, location, type } = req.query;
@@ -93,20 +104,27 @@ const run = async () => {
         const limit = parseInt(req?.query?.limit) || 10;
         const skip = (page - 1) * limit;
         if (title?.length < 3 || location?.length < 3 || type?.length < 3) {
-          return res.status(400).send("Title, location, and type must have at least 3 characters");
+          return res
+            .status(400)
+            .send("Title, location, and type must have at least 3 characters");
         }
         const query = {
           job_title: { $regex: title || "", $options: "i" },
           location: { $regex: location || "", $options: "i" },
           job_type: { $regex: type || "", $options: "i" },
         };
-        const result = await jobsCollection.find(query).skip(skip).limit(limit).toArray();
+        const result = await jobsCollection
+          .find(query)
+          .skip(skip)
+          .limit(limit)
+          .toArray();
         res.send(result);
       } catch (error) {
         res.status(500).send("Server Error");
       }
     });
 
+    //get a single company
     app.get("/company/:id", async (req, res) => {
       try {
         const id = req.params.id;
@@ -118,17 +136,20 @@ const run = async () => {
       }
     });
 
-    app.get('/applied_jobs', async (req, res) => {
+    //get applied jobs data
+    app.get("/applied_jobs/:email", async (req, res) => {
       try {
-        let query = {};
-        if (req.query.email) {
-          query = { candidate_email: req.query.email };
-        }
+        let query = { candidate_email: req.params.email };
+
         const appliedJobs = await appliedJobsCollection.find(query).toArray();
-        const jobIds = appliedJobs.map(job => job.jobId);
-        const jobDetails = await jobsCollection.find({ _id: { $in: jobIds.map(id => new ObjectId(id)) } }).toArray();
-        const appliedJobsWithDetails = appliedJobs.map(appliedJob => {
-          const jobDetail = jobDetails.find(job => job._id.toString() === appliedJob.jobId);
+        const jobIds = appliedJobs.map((job) => job.jobId);
+        const jobDetails = await jobsCollection
+          .find({ _id: { $in: jobIds.map((id) => new ObjectId(id)) } })
+          .toArray();
+        const appliedJobsWithDetails = appliedJobs.map((appliedJob) => {
+          const jobDetail = jobDetails.find(
+            (job) => job._id.toString() === appliedJob.jobId
+          );
           return { ...appliedJob, jobDetail };
         });
         res.send(appliedJobsWithDetails);
@@ -137,7 +158,8 @@ const run = async () => {
       }
     });
 
-    app.get('/bookmark_jobs', async (req, res) => {
+    //get bookmark jobs
+    app.get("/bookmark_jobs", async (req, res) => {
       try {
         let query = {};
         if (req.query.email) {
@@ -150,6 +172,7 @@ const run = async () => {
       }
     });
 
+    //get all companies
     app.get("/companies", async (req, res) => {
       try {
         const page = parseInt(req.query?.page) - 1 || 0;
@@ -159,16 +182,21 @@ const run = async () => {
         if (name) {
           query = { company_name: name };
         }
-        if(req.query.id){
-          query = {_id : new ObjectId(req.query.id)}
+        if (req.query.id) {
+          query = { _id: new ObjectId(req.query.id) };
         }
-        const result = await companiesCollection.find(query).skip(page * size).limit(size).toArray();
+        const result = await companiesCollection
+          .find(query)
+          .skip(page * size)
+          .limit(size)
+          .toArray();
         res.send(result);
       } catch (error) {
         res.status(500).send("Server Error");
       }
     });
 
+    //company count for pagination
     app.get("/company_search", async (req, res) => {
       try {
         const count = await companiesCollection.countDocuments();
@@ -178,36 +206,50 @@ const run = async () => {
       }
     });
 
+    //user data based by email [need to fix role]
     app.get("/user/:email", verifyToken, async (req, res) => {
       try {
         const tokenEmail = req.user?.email;
         const email = req.params.email;
+
         if (tokenEmail && tokenEmail !== email) {
+          console.log("Email mismatch. Forbidden Access!");
           return res.status(403).send({ message: "Forbidden Access!" });
         }
+
         const query = { email: email };
         const result = await usersCollection.findOne(query);
+        if (!result) {
+          return res.status(404).send({ message: "User not found" });
+        }
+
         res.send(result);
       } catch (error) {
+        console.error("Server Error:", error);
         res.status(500).send("Server Error");
       }
     });
 
-    app.get('/role/:email',async(req,res)=>{
+    //check user role
+    app.get("/role/:email", async (req, res) => {
       const email = req.params.email;
-      const result = await usersCollection.findOne({email:email})
-      res.send(result)
-    })
-
-    app.get("/logout", (req, res) => {
-      res.clearCookie("token", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        maxAge: 0,
-      }).send({ success: true });
+      const result = await usersCollection.findOne({ email: email });
+      res.send(result);
     });
 
+    //clearing token
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
+    //insert user to db
     app.post("/user", async (req, res) => {
       try {
         const user = req.body;
@@ -218,22 +260,26 @@ const run = async () => {
       }
     });
 
+    //set cookie to db
     app.post("/auth", async (req, res) => {
       try {
         const user = req.body;
         const token = jwt.sign(user, secret_token, {
           expiresIn: "24h",
         });
-        res.cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        }).send({ success: true });
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
       } catch (error) {
         res.status(500).send("Server Error");
       }
     });
 
+    //add job to db
     app.post("/apply", async (req, res) => {
       try {
         const jobInfo = req.body;
@@ -253,6 +299,7 @@ const run = async () => {
       }
     });
 
+    //add bookmark job
     app.post("/bookmark_jobs", async (req, res) => {
       try {
         const jobInfo = req.body;
@@ -272,13 +319,25 @@ const run = async () => {
       }
     });
 
-    app.patch('/user/:email', async (req, res) => {
+    //create a job to db and ui
+    app.post("/jobs", verifyToken, async (req, res) => {
+      const role = req.user.role;
+      const jobInfo = req.body;
+      if (role !== "company") {
+        return res.send({ message: "Only Company Can Add Job" });
+      }
+      const result = await jobsCollection.insertOne(jobInfo);
+      res.send(result);
+    });
+
+    //update user info
+    app.patch("/user/:email", async (req, res) => {
       try {
         const user = req.body;
         const query = { email: req.params.email };
 
         const updatedUser = {
-          $set: {}
+          $set: {},
         };
 
         for (const key in user) {
@@ -294,7 +353,26 @@ const run = async () => {
       }
     });
 
-    app.delete('/user/:email', async (req, res) => {
+    //update job in db
+    app.patch("/job/:id", async (req, res) => {
+      const job = req.body;
+      const jobId = req.params.id;
+      const query = { _id: new ObjectId(jobId) };
+
+      const updatedJob = {
+        $set: {},
+      };
+      for (const key in job) {
+        if (job.hasOwnProperty(key)) {
+          updatedJob.$set[key] = job[key];
+        }
+      }
+       await jobsCollection.findOneAndUpdate(query,updatedJob)
+      res.send({ success: true })
+    });
+
+    //delete user from db and firebase
+    app.delete("/user/:email", async (req, res) => {
       try {
         const query = { email: req.params.email };
         await usersCollection.deleteOne(query);
@@ -304,10 +382,36 @@ const run = async () => {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+    //cron jobs
+cron.schedule('* * * * *', async () => {
+  try {
+    const currentDate = moment().startOf('day');
+    
+    // Find all jobs with expiration dates before today
+    const expiredJobs = await jobsCollection.find({
+      expiration_date: { $lt: currentDate.format("MMMM D, YYYY") },
+      status: true // Assuming status is a boolean field indicating whether the job is active or not
+    }).toArray();
+    
+    
+    // Update the status of expired jobs
+    for (const job of expiredJobs) {
+      await jobsCollection.updateOne(
+        { _id: new ObjectId(job._id) },
+        { $set: { status: false } } // Set status to false for expired jobs
+      );
+    }
+    
+  } catch (error) {
+    console.error('Error occurred during cron job:', error);
+  }
+});
 
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
   }
 };
 
