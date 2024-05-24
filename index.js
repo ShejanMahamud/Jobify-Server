6d45820a5e28ccadd974cbf19360719be916bd1a
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-05-25 01:42:12 +0600

    SSL-Commerz added for payment

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const moment = require('moment')
 const port = process.env.PORT || 3473;
 const mongoURI = process.env.MONGO_URI;
 const secret_token = process.env.ACCESS_TOKEN_SECRET;
+const SSLCommerzPayment = require('sslcommerz-lts')
+
+//sslcommerz details
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWORD;
+const is_live = false 
 
 // Initialize Express app
 const app = express();
@@ -243,7 +249,7 @@ const run = async () => {
     app.get("/role/:email", async (req, res) => {
       const email = req.params.email;
       const result = await usersCollection.findOne({ email: email });
-      res.send(result);
+      res.send({role: result?.role});
     });
 
     //clearing token
@@ -346,6 +352,49 @@ const run = async () => {
       const result = await jobsCollection.insertOne(jobInfo);
       res.send(result);
     });
+
+    //purchase plan route
+    app.post('/plans',async(req,res)=>{
+const planDetails = req.body;
+const tran_id = new ObjectId().toString()
+const data = {
+  total_amount: planDetails?.price,
+  currency: 'BDT',
+  tran_id: tran_id, // use unique tran_id for each api call
+  success_url: 'http://localhost:3030/success',
+  fail_url: 'http://localhost:3030/fail',
+  cancel_url: 'http://localhost:3030/cancel',
+  ipn_url: 'http://localhost:3030/ipn',
+  shipping_method: 'Courier',
+  product_name: planDetails?.plan,
+  product_category: 'Electronic',
+  product_profile: 'general',
+  cus_name: 'Customer Name',
+  cus_email: 'customer@example.com',
+  cus_add1: 'Dhaka',
+  cus_add2: 'Dhaka',
+  cus_city: 'Dhaka',
+  cus_state: 'Dhaka',
+  cus_postcode: '1000',
+  cus_country: 'Bangladesh',
+  cus_phone: '01711111111',
+  cus_fax: '01711111111',
+  ship_name: 'Customer Name',
+  ship_add1: 'Dhaka',
+  ship_add2: 'Dhaka',
+  ship_city: 'Dhaka',
+  ship_state: 'Dhaka',
+  ship_postcode: 1000,
+  ship_country: 'Bangladesh',
+};
+const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+sslcz.init(data).then(apiResponse => {
+  // Redirect the user to payment gateway
+  let GatewayPageURL = apiResponse.GatewayPageURL
+  res.send({url: GatewayPageURL})
+  console.log('Redirecting to: ', GatewayPageURL)
+});
+    })
 
     //update user info
     app.patch("/user/:email", async (req, res) => {
